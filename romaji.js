cd2f044a32f1a5f40aba3eb4f75f44c9fbee68a6
Gordie Smith <gordie@wistia.com>
2018-03-23 15:48:04 -0400

    convert some romaji

---
+++ romaji.js
@@ -0,0 +1,45 @@
+const convertRomajiToHiragana = function(string) {
+  "use strict"
+
+  let s = '{"n":"ん","a":"あ","i":"い","u":"う","e":"え","o":"お","ka":"か","ki":"き","ku":"く","ke":"け","ko":"こ","sa":"さ","shi":"し","su":"す","se":"せ","so":"そ","ta":"た","chi":"ち","tsu":"つ","te":"て","to":"と","na":"な","ni":"に","nu":"ぬ","ne":"ね","no":"の","ha":"は","hi":"ひ","fu":"ふ","he":"へ","ho":"ほ","ma":"ま","mi":"み","mu":"む","me":"め","mo":"も","ya":"や","yu":"ゆ","yo":"よ","ra":"ら","ri":"り","ru":"る","re":"れ","ro":"ろ","wa":"わ","wi":"ゐ","we":"ゑ","wo":"を","ga":"が","gi":"ぎ","gu":"ぐ","ge":"げ","go":"ご","za":"ざ","ji":"じ","zu":"ず","ze":"ぜ","zo":"ぞ","da":"だ","di":"ぢ","du":"づ","de":"で","do":"ど","ba":"ば","bi":"び","bu":"ぶ","be":"べ","bo":"ぼ","pa":"ぱ","pi":"ぴ","pu":"ぷ","pe":"ぺ","po":"ぽ"}';
+  let hash = JSON.parse(s);
+
+  let output = "";
+  let length = string.length;
+
+  while (string.length > 0) {
+    let test = string.substr(0, 3);
+    if (hash[test]) {
+      output += hash[test];
+      string = string.substring(3);
+      continue;
+    }
+    test = string.substring(0, 2);
+    if (hash[test]) {
+      output += hash[test];
+      string = string.substring(2);
+      continue;
+    }
+    test = string.substring(0, 1);
+    if (hash[test]) {
+      output += hash[test];
+      string = string.substring(1);
+      continue;
+    };
+    if (test === ' ') {
+      string = string.substring(1);
+      continue;
+    }
+    output = "invalid input";
+    break;
+  }
+  console.log(output);
+}
+
+convertRomajiToHiragana('konnichiha');
+convertRomajiToHiragana('oyasuminasai');
+convertRomajiToHiragana('yoroshiku ne');
+convertRomajiToHiragana('kana');
+convertRomajiToHiragana('kan a');
+convertRomajiToHiragana('cheese');
+convertRomajiToHiragana('exodia');
